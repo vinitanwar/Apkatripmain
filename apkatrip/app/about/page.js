a8be1117ad08fd208f0f86e80a88b95044d951e6
jonty1231 <157991083+jonty1231@users.noreply.github.com>
2024-09-30 04:10:53 -0700

    vivek commit

--- apkatrip/app/about/page.js
+++ apkatrip/app/about/page.js
@@ -28,7 +28,7 @@ export default function page() {
               href="/about"
               className="bg-white text-gray-600 py-2 px-6 rounded-lg font-semibold shadow-md hover:bg-gray-100"
             >
-              Learn More
+              Learn 
             </Link>
           </div>
         </section>
