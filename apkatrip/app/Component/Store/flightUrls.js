8a8d137d6c4ffdf8e9c2d45ad9c90dd9d46443e0
jonty1231 <157991083+jonty1231@users.noreply.github.com>
2024-10-01 00:16:33 -0700

    Update flightUrls.js

--- apkatrip/app/Component/Store/flightUrls.js
+++ apkatrip/app/Component/Store/flightUrls.js
@@ -1,3 +1,3 @@
 export const searchFlighturl="https://api.tektravels.com/BookingEngineService_Air/AirService.svc/rest/Search"
 
-export const localurl="http://localhost:8000/api/"
+export const localurl="http://localhost:8000/api/v1"
