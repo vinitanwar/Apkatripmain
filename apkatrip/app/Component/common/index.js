04269b6d20a9499df52e5d15732d309b0814ab0f
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-07 10:21:37 +0530

    add-sss-

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,5 +1,5 @@
-export const apilink =  'http://admin.gursimran.online/api/v1'
+export const apilink =  'https://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
-export const imgurl = 'http://admin.gursimran.online'
+export const imgurl = 'https://admin.gursimran.online'
 
 //http://localhost:8000
