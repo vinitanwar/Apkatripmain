3ba46b47006e8183d68aefa3c415544405bede5b
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-08 11:33:07 +0530

    add-prod

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,5 +1,5 @@
 export const apilink =  'https://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'http://localhost:8000'
-export const development= "";
+export const development= "production";
 //http://localhost:8000
