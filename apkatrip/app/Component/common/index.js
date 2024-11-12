6798d615b7ac7fa05b9e18956077ef223b4e6a27
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-12 15:44:42 +0530

    new-mewww

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,4 +1,4 @@
-export const apilink = 'https://admin.gursimran.online/api/v1'
+export const apilink = 'http://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'https://admin.gursimran.online'
 export const development= "";
