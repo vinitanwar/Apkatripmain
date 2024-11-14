fa44a487709566b6d233dccf5c3d7738178fc7c7
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-14 10:01:30 +0530

    add-prd

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,5 +1,5 @@
 export const apilink = 'https://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'https://admin.gursimran.online/'
-export const development= "";
+export const development= "Production";
 //http://localhost:8000
