93e285fe873dbfa33124260200ed3bda20804fac
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-14 10:08:09 +0530

    sm-pr

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,5 +1,5 @@
 export const apilink = 'https://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'https://admin.gursimran.online/'
-export const development= "Production";
+export const development= "production";
 //http://localhost:8000
