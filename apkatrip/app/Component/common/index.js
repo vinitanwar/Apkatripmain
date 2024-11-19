8e7820eded53b5c5072abe5903b419b8242ea079
jonty1231 <157991083+jonty1231@users.noreply.github.com>
2024-11-19 11:39:03 +0530

    add

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,5 +1,11 @@
-export const apilink = 'https://admin.gursimran.online/api/v1'
+export const apilink = 'http://localhost:8000/api/v1'
 //https://admin.gursimran.online
+<<<<<<< Updated upstream
 export const imgurl = 'https://admin.gursimran.online/'
 export const development= "roduction";
 //http://localhost:8000 
+=======
+export const imgurl = 'http://localhost:8000/'
+export const development= "production";
+//http://localhost:8000
+>>>>>>> Stashed changes
