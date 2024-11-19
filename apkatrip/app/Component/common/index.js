d1970bc09d538cb662309b17ff27ab010ca8545a
jonty1231 <157991083+jonty1231@users.noreply.github.com>
2024-11-19 11:41:53 +0530

    Update index.js

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -2,7 +2,7 @@ export const apilink = 'https://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
 
 export const imgurl = 'https://admin.gursimran.online/'
-export const development= "roduction";
+export const development= "";
 //http://localhost:8000 
 
 
