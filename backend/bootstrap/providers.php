bef703bd892d98b76e380e3471c4a837f9bc5625
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-21 15:48:40 +0530

    Revert "Merge branch 'dev' of https://github.com/vinitanwar/Apkatripmain into dev"
    This reverts commit e2527811fa5428c5117c47de4a17b439ef7f29f2, reversing
    changes made to 2ec32d44e7cbec8854eeb26c529246fd56684015.

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -5,5 +5,6 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\Filament\HotelregPanelProvider::class,
+    App\Providers\Filament\HotelsPanelProvider::class,
     App\Providers\Filament\UserPanelProvider::class,
 ];
