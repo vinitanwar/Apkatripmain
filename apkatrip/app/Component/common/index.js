5d67f2fcaab55f12591bd0ef6414b9c0d265d5aa
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-21 10:44:03 +0530

    add-prd-maoin

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,7 +1,7 @@
 export const apilink = 'https://admin.gursimran.online/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'http://localhost:8000'
-export const development= "Production";
+export const development= "production";
 //http://localhost:8000 
 
 
