4e8c92b29a33c0d5ef930b987cb24c80ecaf8637
Manshu777 <125426162+vinitanwar@users.noreply.github.com>
2024-11-26 11:17:55 +0530

    add-prd-new

--- apkatrip/app/Component/common/index.js
+++ apkatrip/app/Component/common/index.js
@@ -1,7 +1,7 @@
 export const apilink = 'http://localhost:8000/api/v1'
 //https://admin.gursimran.online
 export const imgurl = 'http://localhost:8000'
-export const development= "";
+export const development= "production";
 //http://localhost:8000 
 
 
