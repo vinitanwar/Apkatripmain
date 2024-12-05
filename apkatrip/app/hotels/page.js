3247d4997bfaaa68b8d2fed0d305b3a410003c78
Disha <131692855+Disha-khurana@users.noreply.github.com>
2024-12-05 11:23:25 +0530

    new-me-disha-remove

--- apkatrip/app/hotels/page.js
+++ apkatrip/app/hotels/page.js
@@ -64,32 +64,6 @@ dispatch(getAllRegHotels())
 
 
         <div className="_polrdestnbx mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-<<<<<<< Updated upstream
-  {allReghotels && allReghotels.info.map((dest, index) => (
-    <Link
-      key={index}
-      href={`/hotels/book/${dest.user.slug}`}
-      className="card-container  border border-[#5c6fff] hover:border-[#ffd94f] shadow-lg rounded-lg overflow-hidden transform  transition-transform duration-300 ease-in-out"
-    >
-      <div className="flex gap-4 p-4">
-        <div className="w-2/3 relative">
-          <img
-            src={`${imgurl}/storage/${dest.hotel.hotel_img[0]}`}
-            alt={dest.hotel.hotel_img[0]}
-            className="rounded-md w-full h-52 object-cover"
-          />
-          <div className="absolute top-2 right-2 bg-white opacity-80 px-2 py-1 rounded-full text-xs font-semibold text-gray-700">
-            {dest.hotel.rating} <FaStar className="inline text-yellow-500" />
-          </div>
-          <div className="flex gap-2 mt-2">
-            {dest.hotel.hotel_img.slice(0, 3).map((image, imgIndex) => (
-              <img
-                key={imgIndex}
-                src={`${imgurl}/storage/${image}`}
-                alt={`Hotel Image ${imgIndex}`}
-                className="rounded-md w-14 h-14 object-cover border-2 border-white shadow-sm"
-              />
-=======
           {allReghotels &&
             allReghotels.info.map((dest, index) => (
               <Link
@@ -152,55 +126,9 @@ dispatch(getAllRegHotels())
                   </div>
                 </div>
               </Link>
->>>>>>> Stashed changes
             ))}
         </div>
 
-<<<<<<< Updated upstream
-        <div className="w-1/3 flex flex-col justify-between">
-          <h5 className="text-lg font-semibold mb-1 text-gray-800 truncate">{dest.hotel.property_name}</h5>
-          <p className="text-sm text-gray-600 truncate">
-            <div dangerouslySetInnerHTML={{__html:dest.hotel.address}} >
-
-            </div>
-            </p>
-          <div className="flex gap-1 text-yellow-500">
-            {Array.from({ length: dest.hotel.rating }).map((_, starIndex) => (
-              <FaStar key={starIndex} className="text-lg" />
-            ))}
-          </div>
-
-          <div className="flex flex-col items-end mt-4">
-            <div className="text-lg font-semibold text-black">
-              ₹{dest.hotel.price}
-              <span className="text-sm text-gray-500"> / night</span>
-            </div>
-            <div className="text-sm text-gray-500 line-through">
-              ₹{(Number(dest.hotel.price) + (Number(dest.hotel.price) * 6) / 100)}
-            </div>
-            <button className="mt-3 bg-[#5c6fff] text-white py-2 px-6 rounded-lg hover:bg-[#4a5ccd] transition duration-200">
-            View
-            </button>
-          </div>
-        </div>
-      </div>
-    </Link>
-  ))}
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-=======
->>>>>>> Stashed changes
         <div className="mt-4 text-center">
           <button
             onClick={handleViewAll}
