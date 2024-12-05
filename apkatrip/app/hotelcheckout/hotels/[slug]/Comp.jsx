f679f38708f830bfdc4c9beef345abc2b6acd7a1
jonty1231 <157991083+jonty1231@users.noreply.github.com>
2024-12-05 15:53:45 +0530

    final

--- apkatrip/app/hotelcheckout/hotels/[slug]/Comp.jsx
+++ apkatrip/app/hotelcheckout/hotels/[slug]/Comp.jsx
@@ -1,7 +1,6 @@
 "use client"
-import HotelsComp from '../../Component/AllComponent/formMaincomp/HotelsComp';
 
-import { getAllhotelsapi } from '../../Component/Store/slices/hotelsSlices';
+
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { FaShareAlt, FaStar, FaFilter, FaTimes } from "react-icons/fa";
@@ -13,6 +12,8 @@ import { BiRefresh } from "react-icons/bi";
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { getAllhotelsapi } from '../../../Component/Store/slices/hotelsSlices';
+import HotelsComp from '../../../Component/AllComponent/formMaincomp/HotelsComp';
 
 
 const Comp = ({slug}) => {
